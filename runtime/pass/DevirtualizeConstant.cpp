e07f26efc4274b67cc2a715700d9b6fa3620f22a
jmmartinez <jmmartinez@quarkslab.com>
2018-12-28 13:22:29 +0100

    Devirtualization working back again.

--- runtime/pass/DevirtualizeConstant.cpp
+++ runtime/pass/DevirtualizeConstant.cpp
@@ -68,42 +68,113 @@ static Function* findFunctionAndLinkModules(Module& M, void* HostValue) {
     return nullptr;
 }
 
-bool easy::DevirtualizeConstant::runOnFunction(llvm::Function &F) {
-
-  if(F.getName() != TargetName_)
-    return false;
-
-  llvm::Module &M = *F.getParent();
-
-  for(auto& I: instructions(F)) {
+template<class IIter>
+bool Devirtualize(IIter it, IIter end) {
+  bool Changed = false;
+  for(; it != end; ++it) {
+    Instruction &I = *it;
     auto* VTable = getVTableHostAddress(I);
     if(!VTable)
       continue;
 
     void** RuntimeLoadedValue = *(void***)(uintptr_t)(VTable->getZExtValue());
 
+    void* CalledPtrHostValue = *RuntimeLoadedValue;
+    llvm::Function* F = findFunctionAndLinkModules(*I.getParent()->getParent()->getParent(), CalledPtrHostValue);
+    if(!F)
+      continue;
+
+    Changed = true;
+
     // that's generally the load from the table
     for(User* U : VTable->users()) {
-      ConstantExpr* CE = dyn_cast<ConstantExpr>(U);
-      if(!CE || !CE->isCast())
+      ConstantExpr* Int2Ptr = dyn_cast<ConstantExpr>(U->stripPointerCasts());
+      if(!Int2Ptr)
         continue;
 
-      for(User* U : CE->users()) {
-        if(auto* CalledPtr = dyn_cast<LoadInst>(U)) {
-          void* CalledPtrHostValue = *RuntimeLoadedValue;
-          llvm::Function* Called = findFunctionAndLinkModules(M, CalledPtrHostValue);
-          if(Called) {
-            for(User* U2 : CalledPtr->users()) {
-              if(auto* LI = dyn_cast<LoadInst>(U2))
-                LI->replaceAllUsesWith(Called);
-            }
-          }
-        }
+      for(User* UU : Int2Ptr->users()) {
+        auto* CalledPtr = dyn_cast<LoadInst>(UU);
+        if(!CalledPtr)
+          continue;
+
+        Type* ExpectedTy = CalledPtr->getType()->getContainedType(0);
+        Constant* Called = ConstantExpr::getPointerCast(F, ExpectedTy);
+
+        SmallVector<User*, 4> Users{CalledPtr->user_begin(), CalledPtr->user_end()};
+        for(User* UUU : Users)
+          if(auto* LI = dyn_cast<LoadInst>(UUU))
+            LI->replaceAllUsesWith(Called);
       }
     }
   }
+  return Changed;
+}
 
+bool CastCallWithPointerCasts(FunctionType* CalledTy, FunctionType* UncastedTy) {
+  if(CalledTy->getReturnType() != UncastedTy->getReturnType())
+    return false;
+  if(CalledTy->getNumParams() != UncastedTy->getNumParams())
+    return false;
+  if(CalledTy->isVarArg() != UncastedTy->isVarArg())
+    return false;
+
+  size_t N = CalledTy->getNumParams();
+  for(size_t i = 0; i != N; ++i) {
+    Type* CArgTy = CalledTy->getParamType(i);
+    Type* UArgTy = UncastedTy->getParamType(i);
+    if(CArgTy != UArgTy) {
+      if(!CArgTy->isPointerTy() || !UArgTy->isPointerTy())
+        return false;
+    }
+  }
   return true;
+}
+
+template<class IIter>
+void RecastCalls(IIter it, IIter end) {
+  for(;it != end;) {
+    CallSite CS{&*it++};
+    if(!CS)
+      continue;
+
+    Value* Called = CS.getCalledValue();
+    Value* Uncasted = Called->stripPointerCasts();
+    if(Called == Uncasted)
+      continue;
+
+    FunctionType* CalledTy = cast<FunctionType>(Called->getType()->getContainedType(0));
+    FunctionType* UncastedTy = cast<FunctionType>(Uncasted->getType()->getContainedType(0));
+
+    if(!CastCallWithPointerCasts(CalledTy, UncastedTy))
+      continue;
+
+    CS.setCalledFunction(Uncasted);
+    CS.mutateFunctionType(UncastedTy);
+
+    // cast every pointer argument to the expected type
+    IRBuilder<> B(CS.getInstruction());
+
+    size_t N = CS.getNumArgOperands();
+    for(unsigned i = 0; i != N; ++i) {
+      Value* Arg = CS.getArgOperand(i);
+      Type* ArgTy = Arg->getType();
+      Type* UncTy = UncastedTy->getParamType(i);
+      if(ArgTy->isPointerTy() && ArgTy != UncTy)
+        CS.setArgument(i, B.CreatePointerCast(Arg, UncTy, Arg->getName() + ".recast_calls"));
+    }
+  }
+}
+
+bool easy::DevirtualizeConstant::runOnFunction(llvm::Function &F) {
+
+  if(F.getName() != TargetName_)
+    return false;
+
+  if(Devirtualize(inst_begin(F), inst_end(F))) {
+    RecastCalls(inst_begin(F), inst_end(F));
+    return true;
+  }
+  return false;
 }
 
 static RegisterPass<easy::InlineParameters> X("","",false, false);
